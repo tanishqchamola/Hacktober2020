c4d6df051979ade702c36caa0a47fe872483b5b6
gaurangchamola <71918385+gaurangchamola@users.noreply.github.com>
2020-10-06 13:06:59 +0530

    MSNSADM1 and ICL1902

---
+++ icl1902.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+/*
+int main ()
+{
+    unsigned int t;
+    cin>>t;
+    unsigned int result[t];
+    
+    for (int i=0; i<t; i++)
+    {
+        unsigned int n,count=0;
+        cin>>n;
+        
+        while(n!=0)
+        {   int x;
+            x=sqrt(n);
+            n=n-(x*x);
+            count++;
+        }
+        result[i]=count;
+    }
+    
+    for (int i=0; i<t; i++)
+    {
+        cout<<result[i]<<"\n";
+    }
+    
+}
+*/
